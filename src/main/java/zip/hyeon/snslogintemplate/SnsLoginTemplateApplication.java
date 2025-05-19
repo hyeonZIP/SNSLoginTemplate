5c357316a6eaf7b4e466036422a2414bee5247f2
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-19 16:37:03 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/zip/hyeon/snslogintemplate/SnsLoginTemplateApplication.java
@@ -0,0 +1,13 @@
+package zip.hyeon.snslogintemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsLoginTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsLoginTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zip/hyeon/snslogintemplate/SnsLoginTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package zip.hyeon.snslogintemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsLoginTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
