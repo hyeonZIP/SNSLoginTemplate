4961a3a2ace5de1cbb555a835c76d193145c1ed8
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-21 17:12:32 +0900

    :sparkles: BaseEntity 사용을 위한 JpaAuditing 어노테이션 추가

--- src/main/java/zip/hyeon/snslogintemplate/SnsLoginTemplateApplication.java
+++ src/main/java/zip/hyeon/snslogintemplate/SnsLoginTemplateApplication.java
@@ -2,8 +2,10 @@ package zip.hyeon.snslogintemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SnsLoginTemplateApplication {
 
     public static void main(String[] args) {
