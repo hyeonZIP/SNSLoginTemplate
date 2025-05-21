3941d2de9c6246f69e997ea4d4d215cc420d473b
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-22 01:49:51 +0900

    :sparkles: 에러 코드 추가

--- src/main/java/zip/hyeon/snslogintemplate/exception/ResultCode.java
+++ src/main/java/zip/hyeon/snslogintemplate/exception/ResultCode.java
@@ -8,6 +8,8 @@ public enum ResultCode {
     // 글로벌 1000번대
     SUCCESS(HttpStatus.OK, 0, "정상 처리 되었습니다."),
 
+    // 인증, 인가
+    INVALID_REGISTRATION_ID(HttpStatus.NOT_FOUND, 1000, "유효하지 않은 SNS 로그인"),
     ;
     private final HttpStatus status;
     private final int code;
