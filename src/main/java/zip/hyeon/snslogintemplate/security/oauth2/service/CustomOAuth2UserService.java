b34d77bbc642b9eecf96a5b77c8c01d87ad97d4f
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-26 01:34:56 +0900

    :sparkles: 기존 사용자 조회또는 새로운 사용자 생성 로직 추가

--- src/main/java/zip/hyeon/snslogintemplate/security/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/zip/hyeon/snslogintemplate/security/oauth2/service/CustomOAuth2UserService.java
@@ -1,19 +1,30 @@
 package zip.hyeon.snslogintemplate.security.oauth2.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import zip.hyeon.snslogintemplate.security.oauth2.dto.OAuth2UserDTO;
+import zip.hyeon.snslogintemplate.domain.auth.entity.AuthEntity;
+import zip.hyeon.snslogintemplate.domain.auth.entity.Provider;
+import zip.hyeon.snslogintemplate.domain.auth.repository.AuthRepository;
+import zip.hyeon.snslogintemplate.domain.user.entity.UserEntity;
+import zip.hyeon.snslogintemplate.domain.user.repository.UserRepository;
 import zip.hyeon.snslogintemplate.security.oauth2.dto.CustomOAuth2User;
+import zip.hyeon.snslogintemplate.security.oauth2.dto.OAuth2UserDTO;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+    private final AuthRepository authRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -26,7 +37,44 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         // 3. OAuth2UserDTO 생성
         OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.of(registrationId, attributes);
 
+        // 4. 기존 사용자 조회 또는 새로운 사용자 저장
+        UserEntity userEntity = findOrCreateUser(oAuth2UserDTO);
+
         // 4. OAuth2User 반환
-        return new CustomOAuth2User(oAuth2UserDTO);
+        return new CustomOAuth2User(oAuth2UserDTO, userEntity);
+    }
+
+    /**
+     * Auth 정보 기반으로 기존 사용자 조회 또는 새로운 사용자 생성 및 저장
+     */
+    private UserEntity findOrCreateUser(OAuth2UserDTO oAuth2UserDTO) {
+
+        Provider provider = oAuth2UserDTO.getProvider();
+        Long providerId = oAuth2UserDTO.getProviderId();
+
+        Optional<AuthEntity> authEntity = authRepository.findByProviderAndProviderId(provider, providerId);
+
+        if (authEntity.isPresent()) {
+            return authEntity.get().getUser();
+        }
+
+        return createTempUser(provider, providerId);
+    }
+
+    /**
+     * Auth 객체 생성을 위해 defaultUser 생성
+     */
+    private UserEntity createTempUser(Provider provider, Long providerId) {
+
+        // 1. Auth 객체 생성을 위한 defaultUser 생성 및 저장
+        UserEntity defaultUser = UserEntity.createDefaultUser();
+        UserEntity savedUser = userRepository.save(defaultUser);
+
+        // 2. Auth 객체 생성 및 저장
+        log.info("Auth 객체 생성 및 저장 provider = {}, providerId = {}", provider.getProvider(), providerId);
+        AuthEntity authEntity = AuthEntity.newAuth(provider, providerId, savedUser);
+        authRepository.save(authEntity);
+
+        return savedUser;
     }
 }
