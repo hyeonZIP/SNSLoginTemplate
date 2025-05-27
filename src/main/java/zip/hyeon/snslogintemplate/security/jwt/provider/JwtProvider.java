bac9999978b6b87c8077c286c6f8c62f98df9697
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-27 09:00:51 +0900

    :truck: dto 명 변경

--- src/main/java/zip/hyeon/snslogintemplate/security/jwt/provider/JwtProvider.java
+++ src/main/java/zip/hyeon/snslogintemplate/security/jwt/provider/JwtProvider.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 import zip.hyeon.snslogintemplate.domain.user.entity.UserRole;
 import zip.hyeon.snslogintemplate.security.jwt.JwtKeyManager;
 import zip.hyeon.snslogintemplate.security.jwt.JwtType;
-import zip.hyeon.snslogintemplate.security.jwt.dto.JwtDTO;
-import zip.hyeon.snslogintemplate.security.jwt.dto.JwtRequestDTO;
+import zip.hyeon.snslogintemplate.security.jwt.provider.dto.JwtProviderResponseDTO;
+import zip.hyeon.snslogintemplate.security.jwt.provider.dto.JwtProviderRequestDTO;
 
 import java.util.Date;
 
@@ -22,11 +22,11 @@ public class JwtProvider {
     /*
         AccessToken & RefreshToken 생성
      */
-    public JwtDTO generateAccessTokenAndRefreshToken(JwtRequestDTO dto) {
+    public JwtProviderResponseDTO generateAccessTokenAndRefreshToken(JwtProviderRequestDTO dto) {
         String accessToken = generateToken(dto.getUserId(), dto.getUserRole(), JwtType.ACCESS_TOKEN);
         String refreshToken = generateToken(dto.getUserId(), dto.getUserRole(), JwtType.REFRESH_TOKEN);
 
-        return JwtDTO.of(accessToken, refreshToken);
+        return JwtProviderResponseDTO.of(accessToken, refreshToken);
     }
 
     private String generateToken(Long userId, UserRole role, JwtType token) {
