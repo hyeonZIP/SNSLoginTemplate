a8656e5f17e12df6ab0cc55c9b12e5de3dab8d5e
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-27 09:12:37 +0900

    :recycle: DTO 변경 및 빌더 제거

--- src/main/java/zip/hyeon/snslogintemplate/domain/refreshToken/dto/SaveRefreshTokenDTO.java
+++ src/main/java/zip/hyeon/snslogintemplate/domain/refreshToken/dto/SaveRefreshTokenDTO.java
@@ -2,22 +2,17 @@ package zip.hyeon.snslogintemplate.domain.refreshToken.dto;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class RefreshTokenDTO {
+public class SaveRefreshTokenDTO {
     private Long userId;
     private String refreshToken;
 
-    public static RefreshTokenDTO of(Long userId, String refreshToken) {
-        return RefreshTokenDTO.builder()
-                .userId(userId)
-                .refreshToken(refreshToken)
-                .build();
+    public static SaveRefreshTokenDTO of(Long userId, String refreshToken) {
+        return new SaveRefreshTokenDTO(userId, refreshToken);
     }
 }
