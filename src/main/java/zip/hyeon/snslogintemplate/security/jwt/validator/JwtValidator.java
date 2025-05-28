85f12c3c76bbe6ddc3a40604a357615f93bf61ea
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-28 10:25:50 +0900

    :truck: validator 패키지로 이동

--- src/main/java/zip/hyeon/snslogintemplate/security/jwt/validator/JwtValidator.java
+++ src/main/java/zip/hyeon/snslogintemplate/security/jwt/validator/JwtValidator.java
@@ -1,10 +1,11 @@
-package zip.hyeon.snslogintemplate.security.jwt;
+package zip.hyeon.snslogintemplate.security.jwt.validator;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import zip.hyeon.snslogintemplate.security.jwt.JwtKeyManager;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
