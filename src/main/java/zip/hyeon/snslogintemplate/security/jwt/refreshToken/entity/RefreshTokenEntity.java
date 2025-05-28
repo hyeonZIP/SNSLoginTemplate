e352f734548a1706e7cd127b9f4279a9bcf65752
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-28 14:39:36 +0900

    :card_file_box: 연관관계 수정 및 지연로딩으로 변경

--- src/main/java/zip/hyeon/snslogintemplate/security/jwt/refreshToken/entity/RefreshTokenEntity.java
+++ src/main/java/zip/hyeon/snslogintemplate/security/jwt/refreshToken/entity/RefreshTokenEntity.java
@@ -1,11 +1,12 @@
 package zip.hyeon.snslogintemplate.security.jwt.refreshToken.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -29,7 +30,7 @@ public class RefreshTokenEntity extends BaseEntity {
 
     private String refreshToken;
 
-    @ManyToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
