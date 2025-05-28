11ea7d2cc130dc190fedc57ca35ceef1ddcb40f0
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-28 15:18:08 +0900

    :bulb: 주석 삭제

--- src/main/java/zip/hyeon/snslogintemplate/security/jwt/provider/dto/JwtProviderResponseDTO.java
+++ src/main/java/zip/hyeon/snslogintemplate/security/jwt/provider/dto/JwtProviderResponseDTO.java
@@ -12,12 +12,7 @@ public class JwtProviderResponseDTO {
     private String accessToken;
     private String refreshToken;
 
-    /**
-     * AccessToken과 RefreshToken은 항상 같이 발급된다.
-     * 1. 회원가입시
-     * 2. 리프레쉬 토큰 사용시
-     */
-    public static JwtProviderResponseDTO of(String accessToken, String refreshToken){
-        return new JwtProviderResponseDTO(accessToken,refreshToken);
+    public static JwtProviderResponseDTO of(String accessToken, String refreshToken) {
+        return new JwtProviderResponseDTO(accessToken, refreshToken);
     }
 }
